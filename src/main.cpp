79b4b53d43c5d08a16c14d32a64fb09db772e4dc
Robert <robert.edwards@excession.co>
2021-10-23 15:53:51 +0100

    removes old comments

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@ int main(int argc, char* argv[]) {
     if (params.verbose == true) {
         std::cout << "image: "      << params.image_filename << std::endl;
         std::cout << "algorithm: "  << params.algorithm << std::endl;
-        std::cout << "angle: "      << params.angle << std::endl;  // default broken
-        std::cout << "percent: "    << params.percent << std::endl; // default broken
+        std::cout << "angle: "      << params.angle << std::endl;
+        std::cout << "percent: "    << params.percent << std::endl;
         std::cout << "reverse: "    << params.reverse << std::endl;
         std::cout << "shape: "      << params.shape << std::endl;
     }
