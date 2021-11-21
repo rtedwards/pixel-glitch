de7c2d27882f31ced75a479b878c14f275fee1ac
Robert <robert.edwards@excession.co>
2021-11-21 15:04:50 +0000

    switched to BDD style unittests

--- tests/test_sorting.cpp
+++ tests/test_sorting.cpp
@@ -7,42 +7,44 @@
 #include "pixel.hpp"
 #include "sorting.hpp"
 
-TEST_CASE("Test Sorting", "[sorting, pixel]") {
-    SECTION("Forward") {
-        bool reverse = false;
-        SECTION("A < B") {
-            Pixel pixel_a = { 0x00, 0x00, 0x00, 0x00, 0 };
-            Pixel pixel_b = { 0x00, 0x00, 0x00, 0x00, 255 };
-            REQUIRE(compare_pixels(pixel_a, pixel_b, reverse) == false);
+SCENARIO("Pixels can be compared by value", "[sorting, pixel]") {
+    GIVEN("two Pixels") {
+        WHEN("forward sorting (reverse == false") {
+            bool reverse = false;
+            THEN("pixel_a.value < pixel_b.value") {
+                Pixel pixel_a = { 0x00, 0x00, 0x00, 0x00, 0 };
+                Pixel pixel_b = { 0x00, 0x00, 0x00, 0x00, 255 };
+                REQUIRE(compare_pixels(pixel_a, pixel_b, reverse) == false);
+            }
+            THEN("pixel_a.value > pixel_b.value") {
+                Pixel pixel_a = { 0x00, 0x00, 0x00, 0x00, 255 };
+                Pixel pixel_b = { 0x00, 0x00, 0x00, 0x00, 0 };
+                REQUIRE(compare_pixels(pixel_a, pixel_b, reverse) == true);
+            }
+            THEN("pixel_a.value == pixel_b.value") {
+                Pixel pixel_a = { 0x00, 0x00, 0x00, 0x00, 255 };
+                Pixel pixel_b = { 0x00, 0x00, 0x00, 0x00, 255 };
+                REQUIRE(compare_pixels(pixel_a, pixel_b, reverse) == false);
+            }
         }
-        SECTION("A > B") {
-            Pixel pixel_a = { 0x00, 0x00, 0x00, 0x00, 255 };
-            Pixel pixel_b = { 0x00, 0x00, 0x00, 0x00, 0 };
-            REQUIRE(compare_pixels(pixel_a, pixel_b, reverse) == true);
-        }
-        SECTION("A == B") {
-            Pixel pixel_a = { 0x00, 0x00, 0x00, 0x00, 255 };
-            Pixel pixel_b = { 0x00, 0x00, 0x00, 0x00, 255 };
-            REQUIRE(compare_pixels(pixel_a, pixel_b, reverse) == false);
-        }
-    }
 
-    SECTION("Reverse") {
-        bool reverse = true;
-        SECTION("A < B") {
-            Pixel pixel_a = { 0x00, 0x00, 0x00, 0x00, 0 };
-            Pixel pixel_b = { 0x00, 0x00, 0x00, 0x00, 255 };
-            REQUIRE(compare_pixels(pixel_a, pixel_b, reverse) == true);
-        }
-        SECTION("A > B") {
-            Pixel pixel_a = { 0x00, 0x00, 0x00, 0x00, 255 };
-            Pixel pixel_b = { 0x00, 0x00, 0x00, 0x00, 0 };
-            REQUIRE(compare_pixels(pixel_a, pixel_b, reverse) == false);
-        }
-        SECTION("A == B") {
-            Pixel pixel_a = { 0x00, 0x00, 0x00, 0x00, 255 };
-            Pixel pixel_b = { 0x00, 0x00, 0x00, 0x00, 255 };
-            REQUIRE(compare_pixels(pixel_a, pixel_b, reverse) == false);
+        WHEN("reverse sorting (reverse == true") {
+            bool reverse = true;
+            THEN("pixel_a.value < pixel_b.value") {
+                Pixel pixel_a = { 0x00, 0x00, 0x00, 0x00, 0 };
+                Pixel pixel_b = { 0x00, 0x00, 0x00, 0x00, 255 };
+                REQUIRE(compare_pixels(pixel_a, pixel_b, reverse) == true);
+            }
+            THEN("pixel_a.value > pixel_b.value") {
+                Pixel pixel_a = { 0x00, 0x00, 0x00, 0x00, 255 };
+                Pixel pixel_b = { 0x00, 0x00, 0x00, 0x00, 0 };
+                REQUIRE(compare_pixels(pixel_a, pixel_b, reverse) == false);
+            }
+            THEN("pixel_a.value == pixel_b.value") {
+                Pixel pixel_a = { 0x00, 0x00, 0x00, 0x00, 255 };
+                Pixel pixel_b = { 0x00, 0x00, 0x00, 0x00, 255 };
+                REQUIRE(compare_pixels(pixel_a, pixel_b, reverse) == false);
+            }
         }
     }
 }
